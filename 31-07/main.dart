8f5049b485cfcb7f16a0fcd8c9c3bd7385a22217
celsoeduardobentopugim <140861101+celsoeduardobentopugim@users.noreply.github.com>
2024-07-31 21:58:16 -0300

    Add files via upload

---
+++ 31-07/main.dart
@@ -0,0 +1,16 @@
+void main (){
+  int idade = 23;
+  double altura=1.71;
+  String nome = "Celso Eduardo";
+  bool souBonito = true;
+
+dynamic qualMeuValor="123";
+qualMeuValor = true;
+
+List<String> nomes =['Celso','felipe', 'bruno','luis'];
+
+Map<String, dynamic> registroCliente ={"Nome":"Celso","FormPag":"debito","ValorCompra":200};
+ 
+print(registroCliente["ValorCompra"]);
+
+} 
